b11bfe6de6bb7f07f858b42b52832c3b7e0d8105
hackbrew01 <hackbrew@gmail.com>
2018-02-20 10:36:38 -0500

    Add files via upload

---
+++ lambda/custom/index.js
@@ -0,0 +1,123 @@
+'use strict';
+
+var Alexa = require('alexa-sdk');
+
+var streamInfo = {
+  title: 'Alexa Audio Stream Starter',
+  subtitle: 'A starter template for Alexa audio streaming skills.',
+  cardContent: "Add something for your card to show",
+  url: "Add your URL stream here",
+  image: {
+    largeImageUrl: "Add URL to large image here",
+    smallImageUrl: "Add URL to small image here"
+  }
+};
+
+exports.handler = (event, context, callback) => {
+  var alexa = Alexa.handler(event, context, callback);
+
+  alexa.registerHandlers(
+    handlers,
+    audioEventHandlers
+  );
+
+  alexa.execute();
+};
+
+var handlers = {
+  'LaunchRequest': function() {
+    this.emit('PlayStream');
+  },
+  'PlayStream': function() {
+    this.response.speak('Enjoy.').audioPlayerPlay('REPLACE_ALL', streamInfo.url, streamInfo.url, null, 0);
+    this.emit(':responseReady');
+  },
+  'AMAZON.HelpIntent': function() {
+    // skill help logic goes here
+    this.emit(':responseReady');
+  },
+  'SessionEndedRequest': function() {
+    // no session ended logic needed
+  },
+  'ExceptionEncountered': function() {
+    console.log("\n---------- ERROR ----------");
+    console.log("\n" + JSON.stringify(this.event.request, null, 2));
+    this.callback(null, null)
+  },
+  'Unhandled': function() {
+    this.response.speak('Sorry. Something went wrong.');
+    this.emit(':responseReady');
+  },
+  'AMAZON.NextIntent': function() {
+    this.response.speak('This skill does not support skipping.');
+    this.emit(':responseReady');
+  },
+  'AMAZON.PreviousIntent': function() {
+    this.response.speak('This skill does not support skipping.');
+    this.emit(':responseReady');
+  },
+  'AMAZON.PauseIntent': function() {
+    this.emit('AMAZON.StopIntent');
+  },
+  'AMAZON.CancelIntent': function() {
+    this.emit('AMAZON.StopIntent');
+  },
+  'AMAZON.StopIntent': function() {
+    this.response.speak('Okay. I\'ve stopped the stream.').audioPlayerStop();
+    this.emit(':responseReady');
+  },
+  'AMAZON.ResumeIntent': function() {
+    this.emit('PlayStream');
+  },
+  'AMAZON.LoopOnIntent': function() {
+    this.emit('AMAZON.StartOverIntent');
+  },
+  'AMAZON.LoopOffIntent': function() {
+    this.emit('AMAZON.StartOverIntent');
+  },
+  'AMAZON.ShuffleOnIntent': function() {
+    this.emit('AMAZON.StartOverIntent');
+  },
+  'AMAZON.ShuffleOffIntent': function() {
+    this.emit('AMAZON.StartOverIntent');
+  },
+  'AMAZON.StartOverIntent': function() {
+    this.response.speak('Sorry. I can\'t do that yet.');
+    this.emit(':responseReady');
+  },
+  'PlayCommandIssued': function() {
+
+    if (this.event.request.type === 'IntentRequest' || this.event.request.type === 'LaunchRequest') {
+      var cardTitle = streamInfo.subtitle;
+      var cardContent = streamInfo.cardContent;
+      var cardImage = streamInfo.image;
+      this.response.cardRenderer(cardTitle, cardContent, cardImage);
+    }
+
+    this.response.speak('Enjoy.').audioPlayerPlay('REPLACE_ALL', streamInfo.url, streamInfo.url, null, 0);
+    this.emit(':responseReady');
+  },
+  'PauseCommandIssued': function() {
+    this.emit('AMAZON.StopIntent');
+  }
+}
+
+var audioEventHandlers = {
+  'PlaybackStarted': function() {
+    this.emit(':responseReady');
+  },
+  'PlaybackFinished': function() {
+    this.emit(':responseReady');
+  },
+  'PlaybackStopped': function() {
+    this.emit(':responseReady');
+  },
+  'PlaybackNearlyFinished': function() {
+    this.response.audioPlayerPlay('REPLACE_ALL', streamInfo.url, streamInfo.url, null, 0);
+    this.emit(':responseReady');
+  },
+  'PlaybackFailed': function() {
+    this.response.audioPlayerClearQueue('CLEAR_ENQUEUED');
+    this.emit(':responseReady');
+  }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Alexa Audio Stream Starter - Echo Skill Template
+
+A starter template for creating Alexa audio streaming skills.
